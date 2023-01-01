a22a697320d2a63cd0edea4c1a909e20cfd2b1f4
김성규 <skkim@lifegoeson.kr>
2023-01-01 22:27:37 +0900

    [section.1-create_example] OrderController, Service, Repository V1 추가

---
+++ src/main/java/hello/advanced/app/v1/OrderServiceV1.java
@@ -0,0 +1,33 @@
+package hello.advanced.app.v1;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author skkim
+ * @since 2023/01/01
+ */
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV1 {
+
+    private final OrderRepositoryV1 orderRepository;
+
+    private final HelloTraceV1 trace;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+}
